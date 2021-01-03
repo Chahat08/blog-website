28c02eb257e02f10bb35873087976b4d4406917f
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-03 08:31:44 +0530

    Deploying

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')!m3-8ip5i#a73^f3!$sv^h6%g*^yqpfir^$if!tn^l5m1p^7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
